e13b941e06d1203da89e521f4575a490aa9e762b
5h4hid5 <shahid.bracu@gmail.com>
2017-11-24 10:19:24 +0600

    #2 #32 Add timer with start/pause

--- desktop-app/js/initApp.js
+++ desktop-app/js/initApp.js
@@ -1,13 +1,20 @@
 const remote = require('electron').remote;
-const {ipcRenderer} = require('electron');
+const moment = require('moment');
+const {
+  ipcRenderer
+} = require('electron');
 const dbBackup = require('./js/dbm/dbBackup');
 const processDbm = require('./js/dbm/processDbm');
 const dbpush = require('./js/dbm/sendDatabase');
 const autosave = require('./js/autosave/autosave');
+const timer = require('./js/timer/stopwatch');
 const manageProcess = require('./js/process/manageProcess');
 const mt = require('./js/process/mouseTracker');
 const manageScreenshot = require('./js/screenshot/manageScreenshot');
 
+let play = false,
+  time = 0,
+  hours, mins, secs;
 
 function init(config) {
   renderUI();
@@ -39,9 +46,63 @@ function renderUI() {
     ipcRenderer.send('show-preference');
   });
 
-  document.getElementById("send-data").addEventListener("click",function (e) {
+  document.getElementById("send-data").addEventListener("click", function(e) {
     ipcRenderer.send('show-email-form');
   });
+
+  let timerControl = document.getElementById("timerControls");
+  hours = document.getElementById("hour"),
+    mins = document.getElementById("mins"),
+    secs = document.getElementById("secs"),
+    lastUpdateTime = new Date().getTime(),
+    hourCount = new Date(lastUpdateTime);
+  timerControl.addEventListener('click', () => {
+    if (play) {
+      play = false;
+      playPause();
+    } else {
+      play = true;
+      playPause();
+    }
+  });
+
+}
+
+function playPause() {
+  playPauseBtn = document.getElementById("playPauseBtn");
+  if (play) {
+    playPauseBtn.className = "zmdi zmdi-pause-circle-outline";
+    updateTimer();
+    console.log("Playing");
+  } else {
+    playPauseBtn.className = "zmdi zmdi-play-circle-outline";
+    stopTimer();
+    console.log("Paused");
+  }
+}
+
+function pad(n) {
+  return ('00' + n).substr(-2);
+}
+
+function updateTimer() {
+  if(play){
+    setTimeout(() => {
+      time++;
+      hour.innerHTML = pad(moment.duration(time*1000).hours())+'h';
+      mins.innerHTML = pad(moment.duration(time*1000).minutes())+'m';
+      secs.innerHTML = pad(moment.duration(time*1000).seconds())+'s';
+      updateTimer();
+    }, 1000);
+  }
+}
+
+
+function startTimer() {
+}
+
+function stopTimer() {
+
 }
 
 document.onreadystatechange = function() {

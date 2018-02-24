e24c4b7b22dacdae19715f24908b47fe148b91dd
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2018-02-24 21:23:14 +0600

    trying to solve google api using c# mvc application.

---
+++ feature/R&D-OAuh/Code/GoogleDriveRestApi/GoogleDriveRestAPI-v3/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using GoogleDriveRestAPI_v3.Models;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoogleDriveRestAPI_v3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetGoogleDriveFiles()
+        {
+            return View(GoogleDriveFilesRepository.GetDriveFiles());
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile(GoogleDriveFiles file)
+        {
+            GoogleDriveFilesRepository.DeleteFile(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+         
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            GoogleDriveFilesRepository.FileUpload(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        public void DownloadFile(string id)
+        {
+            string FilePath = GoogleDriveFilesRepository.DownloadGoogleFile(id);
+            
+
+            Response.ContentType = "application/zip";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+            Response.WriteFile(System.Web.HttpContext.Current.Server.MapPath("~/GoogleDriveFiles/" + Path.GetFileName(FilePath)));
+            Response.End();
+            Response.Flush();
+        }
+    }
+}
---
+++ feature/R&D-OAuh/Code/GoogleDriveRestApi/GoogleDriveRestAPI-v3/Models/GoogleDriveFilesRepository.cs
@@ -0,0 +1,168 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Web;
+
+namespace GoogleDriveRestAPI_v3.Models
+{
+    public class GoogleDriveFilesRepository 
+    {
+        public static string[] Scopes = { DriveService.Scope.Drive };
+
+        public static DriveService GetService()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream( @"D:\client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                String FolderPath = @"D:\";
+                String FilePath = Path.Combine(FolderPath, "DriveServiceCredentials.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(FilePath, true)).Result;
+            }
+
+            //Create Drive API service.
+            DriveService service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GoogleDriveRestAPI-v3",
+            });
+
+            return service;
+        }
+
+        public static List<GoogleDriveFiles> GetDriveFiles()
+        {
+            DriveService service = GetService();
+            
+            // Define parameters of request.
+            FilesResource.ListRequest FileListRequest = service.Files.List();
+
+            //listRequest.PageSize = 10;
+            //listRequest.PageToken = 10;
+            FileListRequest.Fields = "nextPageToken, files(id, name, size, version, trashed, createdTime)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = FileListRequest.Execute().Files;
+            List<GoogleDriveFiles> FileList = new List<GoogleDriveFiles>();
+
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    GoogleDriveFiles File = new GoogleDriveFiles {
+                        Id = file.Id,
+                        Name = file.Name,
+                        Size = file.Size,
+                        Version = file.Version,
+                        CreatedTime = file.CreatedTime
+                    };
+                    FileList.Add(File);
+                }
+            }
+            return FileList;
+        }
+
+        public static void FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                DriveService service = GetService();
+
+                string path = Path.Combine(HttpContext.Current.Server.MapPath("~/GoogleDriveFiles"),
+                Path.GetFileName(file.FileName));
+                file.SaveAs(path);
+
+                var FileMetaData = new Google.Apis.Drive.v3.Data.File();
+                FileMetaData.Name = Path.GetFileName(file.FileName);
+                FileMetaData.MimeType = MimeMapping.GetMimeMapping(path);
+
+                FilesResource.CreateMediaUpload request;
+
+                using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+                {
+                    request = service.Files.Create(FileMetaData, stream, FileMetaData.MimeType);
+                    request.Fields = "id";
+                    request.Upload();
+                }
+            }
+        }
+
+        public static string DownloadGoogleFile(string fileId)
+        {
+            DriveService service = GetService();
+
+            string FolderPath = System.Web.HttpContext.Current.Server.MapPath("/GoogleDriveFiles/");
+            FilesResource.GetRequest request = service.Files.Get(fileId);
+
+            string FileName = request.Execute().Name;
+            string FilePath = System.IO.Path.Combine(FolderPath, FileName);
+
+            MemoryStream stream1 = new MemoryStream();
+
+            request.MediaDownloader.ProgressChanged += (Google.Apis.Download.IDownloadProgress progress) =>
+            {
+                switch (progress.Status)
+                {
+                    case DownloadStatus.Downloading:
+                        {
+                            //Console.WriteLine(progress.BytesDownloaded);
+                            break;
+                        }
+                    case DownloadStatus.Completed:
+                        {
+                            //Console.WriteLine("Download complete.");
+                            SaveStream(stream1, FilePath);
+                            break;
+                        }
+                    case DownloadStatus.Failed:
+                        {
+                            //Console.WriteLine("Download failed.");
+                            break;
+                        }
+                }
+            };
+            request.Download(stream1);
+            return FilePath;
+        }
+
+        private static void SaveStream(MemoryStream stream, string FilePath)
+        {
+            using (System.IO.FileStream file = new FileStream(FilePath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                stream.WriteTo(file);
+            }
+        }
+
+        public static void DeleteFile(GoogleDriveFiles files)
+        {
+            DriveService service = GetService();
+            try
+            {
+                // Initial validation.
+                if (service == null)
+                    throw new ArgumentNullException("service");
+
+                if (files == null)
+                    throw new ArgumentNullException(files.Id);
+
+                // Make the request.
+                service.Files.Delete(files.Id).Execute();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Request Files.Delete failed.", ex);
+            }
+        }
+    }
+}

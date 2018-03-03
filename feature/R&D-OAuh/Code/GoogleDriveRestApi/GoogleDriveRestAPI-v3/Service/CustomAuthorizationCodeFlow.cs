a960d001a173f7cf57c3a924eb9a349495e5cb5f
Md. Alim Ul Karim <devorg.bd@gmail.com>
2018-03-04 03:16:42 +0600

    Google Drive API - MVC 5 Application Deployment Error Fix #51
    Google Drive API - MVC 5 Application Deployment Error Fix #51

---
+++ feature/R&D-OAuh/Code/GoogleDriveRestApi/GoogleDriveRestAPI-v3/Service/CustomAuthorizationCodeFlow.cs
@@ -0,0 +1,16 @@
+﻿using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Requests;
+
+namespace GoogleDriveRestAPI_v3.Service {
+    public class CustomAuthorizationCodeFlow : GoogleAuthorizationCodeFlow
+    {
+        public CustomAuthorizationCodeFlow(Initializer initializer)
+            : base(initializer) { }
+
+        public override AuthorizationCodeRequestUrl
+            CreateAuthorizationCodeRequest(string redirectUri)
+        {
+            return base.CreateAuthorizationCodeRequest(CustomAuthorizationBroker.RedirectUri);
+        }
+    }
+}
